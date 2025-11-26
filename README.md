5f49afb3772a0f5be127cbf6dc2661081479b8df
Praneeth-Gandodi <praneethgandhodi@gmail.com>
2025-11-26 23:53:21 +0530

    Add a TUI using rich and made many changes

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-## Y2Api - Youtube and Social media video downloader 
-#Uses yt-dlp as backend
+# YTui
+## Youtube Video/Audio Downloader
+
+A simple Python tool to download videos and audio from YouTube.  
+Built with **yt-dlp** and **Rich** libraries for progress display and better terminal UI.
+
+---
+
+## Note
+⚠️ Do not use this tool for illegal or copyrighted content.  
+⚠️ This project is still under development.  
+⚠️ Videos are downloaded in their original quality and are **not compressed**. Some features may still have **bugs or issues**.
+
+---
+
+## Thanks
+Special thanks to the developers of **[yt-dlp](https://github.com/yt-dlp/yt-dlp)** and **[Rich](https://github.com/Textualize/rich)** for making this project possible.
--- main.py
+++ main.py
@@ -1,102 +1,173 @@
+import os
 import yt_dlp
-#class key
-class ytd:
-    def __init__(self, link):
+from rich import print
+from rich.console import Console
+from rich.panel import Panel
+from rich.progress import Progress, BarColumn, TextColumn, TimeRemainingColumn
+
+console = Console(color_system="auto")
+
+class UnableToProcess(Exception):
+    pass
+
+class CustomProgressbar:
+    def __init__(self, progress, task_id):
+        self.progress = progress
+        self.task_id = task_id
+        self.total_set = False
+
+    def sizeconv(self, sizebytes):
+        if sizebytes is None:
+            return "?"
+
+        value = float(sizebytes)
+        for unit in ["B", "KB", "MB", "GB", "TB"]:
+            if value < 1024:
+                return f"{value:.2f} {unit}"
+            value /= 1024
+
+        return f"{value:.2f} PB"
+
+    def __call__(self, d):
+        if d["status"] == "downloading":
+            downloaded = d.get("downloaded_bytes", 0)
+            total = d.get("total_bytes") or d.get("total_bytes_estimate")
+
+            ##Updating progressbar Manually as Rich can't update progress manually
+            if total and not self.total_set:
+                self.progress.update(self.task_id, total=total)
+                self.total_set = True
+
+            self.progress.update(
+                self.task_id,
+                completed=downloaded,
+                description=f"{self.sizeconv(downloaded)} / {self.sizeconv(total)}"
+            )
+        elif d["status"] == "finished":
+            self.progress.update(
+                self.task_id,
+                completed=self.progress.tasks[self.task_id].total,
+                description="[yellow]Download complete, processing...[/]"
+            )
+        elif d["status"] == "postprocessor":
+            self.progress.update(self.task_id, description="[green]Processing... (FFmpeg)")
+class ProgressManager:
+    def __init__(self):
+        self.progress = Progress(
+            TextColumn("[bold blue]{task.description}[/]"),
+            BarColumn(),
+            TextColumn("{task.percentage:>3.0f}%"),
+            TimeRemainingColumn()
+        )
+    def create_hook(self):
+        task = self.progress.add_task("Starting...", total=None)
+        return self.progress, CustomProgressbar(self.progress, task)
+
+
+class Downloader:
+    def VideoDownload(self, link, address="Downloadstesting"):
         self.link = link
+        self.address = address
 
-    def values(self):
+        pm = ProgressManager()
+        progress, hook = pm.create_hook()
         ydl_opts = {
+            'outtmpl': f'{self.address}/%(title)s.%(ext)s',
             'quiet': True,
-            'download': False
-        }
-        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            info = ydl.extract_info(self.link, download = False)
-            self.title = info['title']
-            self.formats = info['formats']
-            self.duration = info['duration']
-
-
-    def getinfo(self):
-        ydl_opts = {
-            'quiet': True,
-            'download': False
-            }
-        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            info = ydl.extract_info(self.link)
-            print(info['title'])
-            print(info['resolution'])
-
-
-    def VidDown(self):
-        ydl_opts = {
             'noplaylist': True,
-            'format': 'bestvideo/best',
-            'outtmpl': r'C:\Users\prane\Projects\yt-dlp api\Downloads\%(title)s - DOWNLOADED.%(ext)s',
+            'progress': True,
+            "no_warnings": True,
+            "progress_hooks": [hook],
             'postprocessors': [{
             'key': 'FFmpegVideoConvertor',
             'preferedformat': 'mp4',
-            'merge_output_format': 'mp4'
-            }],
+        }],
         }
         try:
-            with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-                ydl.download([self.link])
-        except Exception as e:
-            print("Error")
+            with progress:
+                with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+                    ydl.download([self.link])
+        except UnableToProcess:
+            console.print("Unable to process your request at this time. Please try again later.", style="red",justify="center")
+        else:
+            console.print("Video Downloaded Successfully.", style="green", justify="center")
 
+    def AudioDownload(self, link, address="Downloadtesting"):
+        self.link = link
+        self.address = address
+        pm = ProgressManager()
+        progress, hook = pm.create_hook()
 
-    def audiodownload(self):
         ydl_opts = {
-            'noplaylist': True,
-            'format': 'bestaudio/best',
-            'outtmpl': r'C:\Users\prane\Projects\yt-dlp api\Downloads/%(title)s - DOWNLOADED.%(ext)s',
-            'postprocessors': [{
-                'key': 'FFmpegExtractAudio',
-                'preferredcodec': 'mp3',
-                'preferredquality': '192',
-            }],
+            "quiet": True,
+            "noplaylist": True,
+            "progress": True,
+            "no_warnings": True,
+            "format": "bestaudio/best",
+            "outtmpl": f"{self.address}/%(title)s.%(ext)s",
+            "progress_hooks": [hook],
+            "postprocessors": [
+                {
+                    "key": "FFmpegExtractAudio",
+                    "preferredcodec": "mp3",
+                    "preferredquality": "320",
+                },
+            ],
         }
         try:
-            with yt_dlp.YoutubeDL(ydl_opts) as ydll:
-                ydll.download([self.link])
-        except Exception as e:
-            print("Error Occured",e )
+            with progress:
+                with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+                    ydl.download([self.link])
+        except UnableToProcess:
+            console.print("Unable to process your request at this time. Please try again later.", style="red",justify="center")
+        else:
+            console.print("Audio Downloaded Successfully.", style="green", justify="center")
 
 
 def main():
+    home = os.environ.get("USERPROFILE")
+    default_path = os.path.join(home, "Downloads")
     while True:
-        print("Select download type:")
-        print(" 1. Video")
-        print(" 2. Audio")
-        print(" 3. Info")
+        download = Downloader()
+        console.print(Panel("[purple]Welcome to Youtube Video/Audio Downloader[/][link = https://github.com/Praneeth-Gandodi/Y2Api turquoise2 ] -> [underline]Repo[/underline] <-[/]"), justify="center")
+        print("\n")
+        console.print("Select your options from below :", style="steel_blue1")
+        print()
+        console.print("[royal_blue1]1.[/royal_blue1] Download Video.", style="grey70")
+        console.print("[royal_blue1]2.[/royal_blue1] Download Audio.", style="grey70")
+        console.print("[royal_blue1]3.[/royal_blue1] Download Playlist.", style="grey70")
+        console.print("[royal_blue1]4.[/royal_blue1] Get information from the provided link.", style="grey70")
+        print()
+        value = console.input("[light_sky_blue3]Select an option from those [i]listed above[/i] [Example : 1] [light_sky_blue3]: ")
 
-        try:
-            option = int(input("\nEnter choice (1 or 2 or 3): "))
-        except ValueError:
-            print("Invalid input! Please enter a number.")
-            return
+        if(value == "1"):
+            console.print("[yellow]Note:[/][yellow2] For video downloads, it directly downloads the highest available quality.[/]")
+            link = console.input("[red]Please enter the URL: [/]")
+            path = console.input("[light_sky_blue3]Enter the path to save the file [[yellow]leave empty for default Downloads folder[/yellow]]: [/]")
+            if not path.strip():
+                path = default_path
 
-        link = input("Enter URL: ").strip()
-
-        downloader = ytd(link)
-        downloader.values()
-
-        for f in downloader.formats:
-            if f.get("resolution"):
-                print(f["resolution"])
-
-        if option == 1:
-            downloader.VidDown()
-        elif option == 2:
-            downloader.audiodownload()
-        elif option == 3:
-            downloader.getinfo()
+            download.VideoDownload(link, path)
+        elif(value == "2"):
+            link = console.input("[red]Please enter the URL: [/]")
+            path = console.input("[light_sky_blue3]Enter the path to save the file [[yellow]leave empty for default Downloads folder[/yellow]]: [/]")
+            if not path.strip():
+                path = default_path
+            download.AudioDownload(link, path)
+        elif(value == "3"):
+            console.print("[red] The feature 'Download Playlist' is still Under Construction.[/]")
         else:
-            print("Invalid option! Please enter 1 or 2.")
+            console.print("These features coming soon")
+
 
 
 if __name__ == "__main__":
     try:
         main()
+    except KeyboardInterrupt as e:
+        print()
+        console.print("Thanks for using the downloader.", style="red", justify="center")
     except Exception as e:
-        print("Error occurred in the main function")
+        console.print_exception(show_locals=True)
+        console.print("An unexpected error occurred.", e, style="red", justify="center")
+
